a058e5c7ea576108c8a7798d6e6fc604dfa62bc9
andresperezfc <felipe3apc@gmail.com>
2020-07-20 16:10:31 -0500

    input de añadir locacion terminado

--- lib/src/place/ui/screens/add_place_screen.dart
+++ lib/src/place/ui/screens/add_place_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:platzitrips/src/place/ui/widgets/title_input_location.dart';
 import 'package:platzitrips/src/widgets/gradient_back.dart';
+import 'package:platzitrips/src/widgets/text_input.dart';
 import 'package:platzitrips/src/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -16,6 +18,9 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -51,6 +56,36 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                 ),
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120, bottom: 20),
+            child: ListView(
+              children: <Widget>[
+                Container(),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: TextInput(
+                    hintText: "Titulo",
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                TextInput(
+                  hintText: "Descripción",
+                  inputType: TextInputType.multiline,
+                  maxLines: 4,
+                  controller: _controllerDescriptionPlace,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: TextInputLocation(
+                    hintText: "Añadir locación",
+                    iconData: Icons.place,
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
